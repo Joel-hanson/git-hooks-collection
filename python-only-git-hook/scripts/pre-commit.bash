2ff687b48f8ede382ff4e752b1dfa6f75c38bb66
Joel Hanson <joelhanson025@gmail.com>
2019-08-21 12:24:33 +0530

    The pre-commit hook change

--- python-only-git-hook/scripts/pre-commit.bash
+++ python-only-git-hook/scripts/pre-commit.bash
@@ -5,8 +5,13 @@ echo "Running pre-commit hook"
 
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
+    echo ""
+    echo ""
     echo "............................"
     echo ""
-    echo "Tests must pass before commit!"
+    echo "Tests must be passed before you commit!"
+    echo ""
+    echo "............................"
+    echo ""
     exit 1
 fi
